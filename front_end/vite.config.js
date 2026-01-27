0b5706babac366c9513ebba100d83a9efc706cec
Liam Newsam <liamcnewsam@gmail.com>
2026-01-27 15:33:00 -0800

    changing

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/"
+})
+
+/* 
+
+export default defineConfig({
+  plugins: [react()],
   base: process.env.NODE_ENV === "production"
     ? "/projects/w101/w101-demo/"
     : "/",   // Important!
 })
+
+*/
