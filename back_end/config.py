329314392c95fa83465db124909a3e4425d7a080
Liam Newsam <liamcnewsam@gmail.com>
2026-01-28 15:50:47 -0800

    dumb

--- back_end/config.py
+++ back_end/config.py
@@ -3,7 +3,7 @@
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "devsecret123")
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite://game.db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///game.db")
 JWT_EXPIRY_SECONDS = 60 * 60 * 2  # 24 hours
 GUEST_SESSION_LIFETIME = 60 * 60  # 1 hour
 
