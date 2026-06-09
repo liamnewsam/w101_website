34f5ee820ec9d57e0dafc076461d1d422ca9c0f1
Liam Newsam <liamcnewsam@gmail.com>
2026-06-09 16:24:44 -0700

    changing something

--- back_end/models.py
+++ back_end/models.py
@@ -19,7 +19,7 @@ def _elo_to_level(elo: int) -> int:
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
